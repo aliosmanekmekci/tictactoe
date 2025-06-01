9aba3f86ec03956368b45755db0b8b9fcb63783a
Ali Ekmekci <aliosmanekmekci@gmail.com>
2025-06-01 15:16:42 +0300

    Add private check conditions to GameController

--- script.js
+++ script.js
@@ -29,6 +29,37 @@ const GameController = (function () {
   let currentPlayerIndex;
   let isGameOver = false;
 
+  const _checkWin = () => {
+    const currentBoard = Gameboard.getBoard();
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winConditions.length; i++) {
+      const [a, b, c] = winConditions[i];
+      if (currentBoard[a] && currentBoard[a] === currentBoard[b] && currentBoard[a] === currentBoard[c]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const _checkTie = () => {
+    const currentBoard = Gameboard.getBoard();
+    return currentBoard.every((cell) => cell !== "") && !_checkWin();
+  };
+
+  const _switchTurn = () => {
+    currentPlayerIndex = 1 - currentPlayerIndex;
+    console.log(`It's ${players[currentPlayerIndex].name}'s turn ${players[currentPlayerIndex].marker}.`);
+  };
+
   const startGame = () => {
     players = [createPlayer("Player 1", "X"), createPlayer("Player 2", "O")];
     currentPlayerIndex = 0;
